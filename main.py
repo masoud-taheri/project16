f8ceec73a5db61b75d944157989d1f99ce2c3379
masoud-taheri <masoud.taheri.2008@gmail.com>
2024-06-14 17:46:55 +0330

    second changes

--- main.py
+++ main.py
@@ -2,6 +2,5 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 
-driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
 
 driver.get("https://google.com")
